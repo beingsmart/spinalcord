fe9c0cbb864c55c948b8f6509a39d070ea3e0307
Sarath Chandra J <sarath@peel.com>
2016-10-10 12:20:56 +0530

    changing event type

--- src/main/java/events/EventType.java
+++ src/main/java/events/EventType.java
@@ -7,6 +7,7 @@ public enum EventType {
     UNIQUE_PARKING_LOCATIONS("UNIQUE_PARKING_LOCATIONS"),
     USER_ACTIVITY("user_activity"),
     PARKING("DEETS"),
+    VACATING("DEETS"),
     AD_CLICK("DEETS"),
     APP_OPEN("DEETS");
 
